2a0481079dc18a373aa095914bfa70183a6a40f6
Marcio <devmarciofaria@gmail.com>
2022-11-15 18:49:00 -0300

    add bootstrap and create navbar

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,15 @@
+
+function Navigation(){
+  return(
+    <nav className='navbar navbar-dark bg-dark container-fluid'>
+      <ul>
+        <li >
+          <button className="btn btn-primary">Switch Language</button>
+        </li>
+        
+      </ul>
+    </nav>
+  )
+}
+
+export {Navigation}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,29 @@
-import styles from '../styles/Home.module.scss'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useTranslate } from '../hooks/useTranslante';
+import styles from "../styles/Home.module.scss";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslate } from "../hooks/useTranslante";
+import { Navigation } from "../components/Navigation";
 
 export default function Home() {
-  const {commonTranslations} = useTranslate()
-console.log(commonTranslations)
-  
+  const { commonTranslations } = useTranslate();
+
   return (
-    <section className={styles.container}>
-      <h1>{commonTranslations('sectionTitle')}</h1>
-    </section>
-  )
+    <>
+     <Navigation/>
+   
+     <main>
+      <section className={styles.container}>
+        <h1>{commonTranslations("sectionTitle")}</h1>
+      </section>
+    </main>
+    </>
+   
+  );
 }
 
-export async function getStaticProps({locale}:any){
- 
+export async function getStaticProps({ locale }: any) {
   return {
-    props:{
-      ...(await serverSideTranslations(locale,['common'])),
-
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
     },
   };
 }
-
